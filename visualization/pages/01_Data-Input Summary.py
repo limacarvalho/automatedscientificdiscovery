3ce000de624288c7ae0c8a1d5eec6545e90e21fa
Gerrit Tombrink <38421759+Gtombri@users.noreply.github.com>
2022-12-06 20:43:46 +0100

    Upload streamlit app

--- visualization/pages/01_Data-Input Summary.py
+++ visualization/pages/01_Data-Input Summary.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 import io
 
 st.set_page_config(layout="wide")
@@ -10,7 +12,7 @@ st.subheader("Your chosen dataset displayed as a table:")
 st.dataframe(df_input)
 st.markdown("")
 st.markdown("")
-st.subheader("Basic statistical analysis of your chosen dataset:")
+st.subheader("Basic Analysis:")
      
 tab1, tab2, tab3, tab4 = st.tabs(["Data information", "Basic statistics", "NA values", "Duplicated values"])
 
@@ -28,3 +30,10 @@ with tab3:
 with tab4:
         duplicated_val = df_input.apply(lambda x: x.duplicated()).sum().to_frame('Duplicated values')
         st.write(duplicated_val)
+st.markdown("")
+st.markdown("")
+st.markdown("***")
+st.subheader("Exploratory Data Analysis:")
+if st.button('Generate EDA-Report'):
+        profile=ProfileReport(df_input, minimal=True, explorative=True, title="Uploaded dataset", progress_bar=True) 
+        st_profile_report(profile)
