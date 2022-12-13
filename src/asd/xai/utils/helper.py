5371a604d0862c10c08a180d4c013e2ef4556ccb
Wasif Masood <rwmasood@gmail.com>
2022-12-13 12:45:45 +0100

    reshaped some files

--- src/asd/xai/utils/helper.py
+++ src/asd/xai/utils/helper.py
@@ -1,46 +1,9 @@
 import torch
 import numpy as np
-import pandas as pd
-from pathlib import Path
-import datatable as dt
-import pandas as pd
-from .utilities_asd_team  import convert_datatypes
 import warnings
 warnings.filterwarnings('ignore')
 
 
-
-def get_covid_dataset():
-    df = pd.read_csv('/mnt/c/Users/rwmas/GitHub/data/covid/20220319_covid_merge_processed.csv', sep=",")
-    X = df[df.columns[df.columns!='y']]
-    # X = X.drop(DROP_LIST, axis = 1)
-    y = df[df.columns[df.columns=='y']]
-    return X, y
-
-
-
-def get_tokamat_dataset():
-    
-    ## read data table
-    base_path = Path(__file__).parent
-    print(base_path)
-    # HDB5.2.3_nans.csv,
-    path = ('/mnt/c/Users/rwmas/GitHub/data/tokamak/HDB5.2.3_no_nans.csv')#.resolve()
-    data = dt.fread(path).to_pandas()
-
-    ## important: convert datatypes first!
-    data = convert_datatypes(data)
-    # print('success:', data.shape, '\n', data.dtypes)
-    return data
-
-
-
-#def get_dd_covid_dataset():
-#    df = dd.read_csv('/home/wasif/python-asd/xai/app/test/data/20220319_covid_merge_processed.csv', sep=",")
-#    X = df[df.columns[df.columns!='y']]
-#    # X = X.drop(DROP_LIST, axis = 1)
-#    y = df[df.columns[df.columns=='y']]
-#    return X, y
 
 
 # determine the supported device
