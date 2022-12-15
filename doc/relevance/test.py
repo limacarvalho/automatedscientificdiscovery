e662c6ad7d2c5b08f8c0d93cf5fe99d199fb146b
wasif.masood <rwmasood@gmail.com>
2022-12-15 14:04:55 +0100

    changed the name of the root xai folder to relevance

--- doc/relevance/test.py
+++ doc/relevance/test.py
@@ -1,1 +1,3 @@
 from asd import relevance
+
+
--- src/asd/__init__.py
+++ src/asd/__init__.py
@@ -4,5 +4,3 @@ from .predictability.utils import get_column_combinations
 from .predictability.utils import tuple_selection
 from .predictability.utils import plot_result as predictability_plot
 
-
-from .xai.relevance import relevance
