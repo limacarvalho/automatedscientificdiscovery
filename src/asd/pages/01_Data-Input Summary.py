908fef580568188d18eaa9b457eb57ae0382ee2c
Joao Carvalho <joao@limacarvalho.com>
2023-01-05 16:51:48 +0000

    Small code refactoring for data input page

--- src/asd/pages/01_Data-Input Summary.py
+++ src/asd/pages/01_Data-Input Summary.py
@@ -1,9 +1,10 @@
 # Import libraries
-import streamlit as st
+import io
+
 import pandas as pd
+import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
-import io
 
 # Set streamlit layout
 st.set_page_config(layout="wide")
@@ -25,7 +26,7 @@ if file_upload is not None:
     st.write("")
     st.write("")
     df_input = pd.read_csv(file_upload)
-    st.session_state["df_input"] = df_input        
+    st.session_state["df_input"] = df_input
     st.write("As a brief overview, your dataset si displayed:")
     st.dataframe(df_input)
     st.markdown("")
@@ -36,15 +37,15 @@ elif "df_input" not in st.session_state:
     st.write("")
     df_input = pd.read_csv(r"datasets/20220727_covid_159rows_52cols_2020.csv")
     st.session_state["df_input"] = df_input
-    st.write("As a brief overview, the default dataset (Covid) is displayed:")  
+    st.write("As a brief overview, the default dataset (Covid) is displayed:")
     st.dataframe(df_input)
     st.markdown("")
     st.markdown("")
-elif "df_input" in st.session_state:   
+elif "df_input" in st.session_state:
     df_input = st.session_state["df_input"]
     st.write("Your chosen dataset is used.")
     st.write("")
-    st.write("")     
+    st.write("")
     st.write("As a brief overview, your dataset si displayed:")
     st.dataframe(df_input)
     st.markdown("")
@@ -53,23 +54,23 @@ elif "df_input" in st.session_state:
 # Implement mainframe output
 st.subheader("Basic Analysis:")
 
-# Implement tabs with different calculations     
+# Implement tabs with different calculations
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["Data information", "Basic statistics", "NA values", "Duplicated values", "Report"])
 
 with tab1:
-        buffer = io.StringIO()
-        df_input.info(buf=buffer)
-        i = buffer.getvalue()
-        st.text(i)
+    buffer = io.StringIO()
+    df_input.info(buf=buffer)
+    i = buffer.getvalue()
+    st.text(i)
 with tab2:
-        st.dataframe(df_input.describe())
+    st.dataframe(df_input.describe())
 with tab3:
-        NA_val = df_input.isnull().sum().to_frame('NA values')
-        NA_val.index.names = ["Column name"]
-        st.write(NA_val)
+    NA_val = df_input.isnull().sum().to_frame("NA values")
+    NA_val.index.names = ["Column name"]
+    st.write(NA_val)
 with tab4:
-        duplicated_val = df_input.apply(lambda x: x.duplicated()).sum().to_frame('Duplicated values')
-        st.write(duplicated_val)
+    duplicated_val = df_input.apply(lambda x: x.duplicated()).sum().to_frame("Duplicated values")
+    st.write(duplicated_val)
 with tab5:
-        profile=ProfileReport(df_input, minimal=True, explorative=True, title="Uploaded dataset", progress_bar=True) 
-        st_profile_report(profile)
+    profile = ProfileReport(df_input, minimal=True, explorative=True, title="Uploaded dataset", progress_bar=True)
+    st.button("Generate Report", on_click=st_profile_report, args=[profile])
